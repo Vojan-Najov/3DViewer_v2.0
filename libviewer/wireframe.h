b7899ac34612a6805e75d2f83fb3487cfcdb5d03
Vojan-Najov <serejah.mashichev@gmail.com>
2024-01-16 02:24:01 +0300

    Update wireframe

--- libviewer/wireframe.h
+++ libviewer/wireframe.h
@@ -1,6 +1,7 @@
 #ifndef WIREFRAME_H_
 #define WIREFRAME_H_
 
+#include <iostream>
 #include <vector>
 
 #include "scene_object.h"
@@ -23,21 +24,29 @@ class Wireframe : public SceneObject<T> {
   Wireframe &PushBack(const Vertex<T> &v);
 
  public:
+  size_t VerticesNumber(void) const override;
+
+ public:
   void Transform(const Matrix4x4<T> &transform) override;
 
  private:
-	std::vector<Vertex<T>> verteces_;
+	std::vector<Vertex<T>> vertices_;
 };
 
 template <typename T>
 inline Wireframe<T> &Wireframe<T>::PushBack(const Vertex<T> &v) {
-	verteces_.push_back(v);
+	vertices_.push_back(v);
 	return *this;
 }
 
 template <typename T>
 inline void Wireframe<T>::Transform(const Matrix4x4<T> &m) {
 	(void) m;
+}
+
+template <typename T>
+inline size_t Wireframe<T>::VerticesNumber(void) const {
+	return vertices_.size();
 }
 
 }  // namespace s21
