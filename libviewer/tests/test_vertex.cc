13232a792074ad4f590d3cb1029ce793a109f4b4
Vojan-Najov <serejah.mashichev@gmail.com>
2024-01-15 21:03:56 +0300

    Add some tests for vertex

--- libviewer/tests/test_vertex.cc
+++ libviewer/tests/test_vertex.cc
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 
 #include "vertex.h"
+#include "matrix4x4.h"
 
 class VertexTest : public testing::Test {};
 
@@ -84,6 +85,84 @@ TEST_F(VertexTest, OperatorMultAssign) {
 	EXPECT_EQ(v[3], 1.0);
 }
 
+TEST_F(VertexTest, Test1) {
+    s21::Vertex<double> p = {1, 2, 3};
+    s21::Matrix4x4<double> m = {
+        {1, 0, 0, 0},
+        {0, 1, 0, 0},   
+        {0, 0, 1, 0},
+        {0, 0, 0, 1}
+    };
+    s21::Vertex<double> p2 = p * m;
+    EXPECT_NEAR(p2[0], 1., 1e-7);
+    EXPECT_NEAR(p2[1], 2., 1e-7);
+    EXPECT_NEAR(p2[2], 3., 1e-7);
+    EXPECT_NEAR(p2[3], 1., 1e-7);
+}
+
+TEST_F(VertexTest, Test2) {
+    s21::Vertex<double> p = {1, 2, 3};
+    s21::Matrix4x4<double> m = {
+        {0.5, 0, 0, 0},
+        {0, 0.5, 0, 0},
+        {0, 0, 0.5, 0},
+        {0, 0, 0, 0.5}
+    };
+    p *= m;
+    EXPECT_NEAR(p.x(), 0.5, 1e-7);
+    EXPECT_NEAR(p.y(), 1, 1e-7);
+    EXPECT_NEAR(p.z(), 1.5, 1e-7);
+    EXPECT_NEAR(p.w(), 0.5, 1e-7);
+}
+
+TEST_F(VertexTest, Test3) {
+    s21::Vertex<double> p = {1, 2, 3};
+    s21::Vertex<double> p2 = p;
+    p *= 2;
+    p = p * 2;
+    EXPECT_NEAR(p[0], 4, 1e-7);
+    EXPECT_NEAR(p[1], 8, 1e-7);
+    EXPECT_NEAR(p[2], 12, 1e-7);
+    EXPECT_NEAR(p[3], 1., 1e-7);
+    EXPECT_NEAR(p2[0], 1, 1e-7);
+    EXPECT_NEAR(p2[1], 2, 1e-7);
+    EXPECT_NEAR(p2[2], 3, 1e-7);
+    EXPECT_NEAR(p2[3], 1., 1e-7);
+}
+
+TEST_F(VertexTest, Test4) {
+    s21::Vertex<double> p = {1, 2, 3, 4};
+    s21::Vertex<double> p2 = p + p;
+    p2 += p;
+    EXPECT_NEAR(p2[0], 3, 1e-7);
+    EXPECT_NEAR(p2[1], 6, 1e-7);
+    EXPECT_NEAR(p2[2], 9, 1e-7);
+    EXPECT_NEAR(p2[3], 12, 1e-7);
+    EXPECT_NEAR(p[0], 1, 1e-7);
+    EXPECT_NEAR(p[1], 2, 1e-7);
+    EXPECT_NEAR(p[2], 3, 1e-7);
+    EXPECT_NEAR(p[3], 4, 1e-7);
+}
+
+TEST_F(VertexTest, Test5) {
+    s21::Vertex<double> p = {1, 2, 3, 4};
+    p.x() = 2;
+    EXPECT_NEAR(p[0], 2, 1e-7);
+}
+
+TEST_F(VertexTest, Test6) {
+    s21::Vertex<double> p = s21::Matrix4x4<double>({
+        {1, 0, 0, 0},
+        {0, 2, 0, 0},
+        {0, 0, 3, 0},
+        {0, 0, 0, 4}
+    });
+    EXPECT_NEAR(p[0], 1, 1e-7);
+    EXPECT_NEAR(p[1], 2, 1e-7);
+    EXPECT_NEAR(p[2], 3, 1e-7);
+    EXPECT_NEAR(p[3], 4, 1e-7);
+}
+
 TEST_F(VertexTest, Input01) {
 	std::istringstream iss{"v 1 2 3"};
 	s21::Vertex<float> v;
@@ -105,3 +184,4 @@ TEST_F(VertexTest, Input02) {
 	EXPECT_EQ(v[2], 3.0);
 	EXPECT_EQ(v[3], 1.0);
 }
+
