9c09c83a0b9215a5474eac79ec47f6cb1b7896f4
Vojan-Najov <serejah.mashichev@gmail.com>
2024-01-17 13:32:07 +0300

    Fix scene_object

--- libviewer/scene_object.h
+++ libviewer/scene_object.h
@@ -5,10 +5,11 @@
 
 namespace s21 {
 
-template <typename T>
+template <typename T, typename U>
 class SceneObject {
  public:
   virtual size_t VerticesNumber(void) const = 0;
+  virtual size_t FacesNumber(void) const = 0;
   virtual void Transform(const Matrix4x4<T> &m) = 0;
   virtual ~SceneObject(void) {}
 };
