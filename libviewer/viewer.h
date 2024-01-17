3410134ba9fc768cd0e6a2e3a8396696a8550d8f
Vojan-Najov <serejah.mashichev@gmail.com>
2024-01-17 13:36:23 +0300

    Add viewer class

--- libviewer/viewer.h
+++ libviewer/viewer.h
@@ -3,6 +3,8 @@
 
 #include "scene.h"
 
+namespace s21 {
+
 class ViewerOperationResult final {
  public:
   bool isSuccesses(void) const { return !errorMessage.empty(); }
@@ -24,5 +26,7 @@ class Viewer final {
  private:
   Scene<float> scene_;
 };
+
+}  // namespace s21
 
 #endif  // LIBVIEWER_VIEWER_H_
