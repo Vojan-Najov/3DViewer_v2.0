d4bc96d9d7e944cf875fa3cafeeed2260b8d55b5
ccartman <ccartman@student.21-school.ru>
2024-01-21 19:08:24 +0300

    fix render_ffp

--- libviewer/render_ffp.h
+++ libviewer/render_ffp.h
@@ -47,10 +47,10 @@ void RenderFFP<T, U>::DrawScene(Scene<T, U> &scene, int width, int height) {
     const Figure<T, U> &figure = scene.GetFigure();
     glLoadIdentity();
     glVertexPointer(4, GL_FLOAT, 0, figure.GetVertices().data());
-    // SetViewMatrix(scene);
+    SetViewMatrix(scene);
     SetProjectionMatrix(scene, width, height);
     DrawVertices(scene);
-    // DrawFaces(scene);
+    DrawFaces(scene);
   }
 
   glFlush();
@@ -62,13 +62,16 @@ void RenderFFP<T, U>::SetViewMatrix(Scene<T, U> &scene) {
   glLoadIdentity();
 
   if (!scene.IsEmpty()) {
-    // Vertex<T> = scene.GetCamera();
+    Vertex<T> camera= scene.GetCamera();
     Figure<T, U> figure = scene.GetFigure();
-    T dim = figure.GetDimension();
     Vertex<T> centre = figure.GetCentre();
-    Vertex<T> camera{centre.x(), centre.y(), centre.z() + 2 * dim};
-    // glTranslatef(-camera.x(), -camera.y(), -camera.z());
-    // glTranslatef(centre.x(), centre.y(), centre.z());
+
+    glTranslatef(-camera.x(), -camera.y(), -camera.z());
+    (void) centre;
+    //glTranslatef(model->centre[0], model->centre[1], model->centre[2]);
+    //glRotatef(xRot, 1.0f, 0.0f, 0.0f);
+    //glRotatef(yRot, 0.0f, 1.0f, 0.0f);
+    //glTranslatef(-model->centre[0], -model->centre[1], -model->centre[2]);
   }
 }
 
@@ -78,78 +81,26 @@ void RenderFFP<T, U>::SetProjectionMatrix(Scene<T, U> &scene, int width,
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
 
-  T x_aspect = width > height ? (float)width / (float)height : 1.0;
-  T y_aspect = height > width ? (float)height / (float)width : 1.0;
 
   if (!scene.IsEmpty()) {
-    // Vertex<T> = scene.GetCamera();
+    T x_aspect = width > height ? (float)width / (float)height : 1.0;
+    T y_aspect = height > width ? (float)height / (float)width : 1.0;
+    
+    Vertex<T> camera = scene.GetCamera();
     Figure<T, U> figure = scene.GetFigure();
-    T dim = figure.GetDimension();
     Vertex<T> centre = figure.GetCentre();
-    Vertex<T> camera{centre.x(), centre.y(), centre.z() + 2 * dim};
-    // glTranslatef(-camera.x(), -camera.y(), -camera.z());
-    // glTranslatef(centre.x(), centre.y(), centre.z());
+    T dim = figure.GetDimension();
 
     T ro = centre[2] - camera[2];
-    T focal_ro = dim > 1.0e-6 ? dim / 10 : 1;
-
+    T focal_ro = scene.GetCameraFocalRo();
     T near = focal_ro;
-    near = -10;
-    T far = -100;
-    // T far = -ro + dim > near ? -ro + dim : 2 * near;
+    T far = -ro + dim > near ? -ro + dim : 2.0f * near;
     ro = ro < 0 ? -ro : focal_ro;
-
-    std::cout << "centre " << centre[0] << " " << centre[1] << " " << centre[2]
-              << '\n';
-    std::cout << "left: " << -ro * x_aspect << '\n';
-    std::cout << "right: " << ro * x_aspect << '\n';
-    std::cout << "bottom: " << -ro * y_aspect << '\n';
-    std::cout << "up: " << ro * y_aspect << '\n';
-    std::cout << "near: " << near << '\n';
-    std::cout << "far: " << far << '\n';
-
-    // glOrtho(-ro * x_aspect, ro * x_aspect, -ro * y_aspect, ro * y_aspect,
-    //         near, far);
-    glOrtho(-100., 100., -100., 100., -100., 100.);
+    
+    glOrtho(-ro * x_aspect, ro * x_aspect, -ro * y_aspect, ro * y_aspect,
+            near, far);
   }
 }
-
-/*
-static void set_projection_matrix(int width, int height) {
-  vw_model_t *model;
-  vw_point_t camera;
-  int projection_type;
-  float focal_ro, ro, near, far;
-  float x_aspect, y_aspect;
-
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-
-  x_aspect = width > height ? (float)width / (float)height : 1.0;
-  y_aspect = height > width ? (float)height / (float)width : 1.0;
-
-  vw_scene_get_camera(&camera[0], &camera[1], &camera[2]);
-  model = vw_scene_get_model();
-  if (model != NULL) {
-    ro = model->centre[2] - camera[2];
-    projection_type = vw_scene_get_projection_type();
-    focal_ro = vw_scene_get_focal_distance();
-    if (projection_type == VW_PARALLEL_PROJECTION) {
-      near = focal_ro;
-      far = -ro + model->dim > near ? -ro + model->dim : 2.0f * near;
-      ro = ro < 0.0f ? -ro : focal_ro;
-
-      glOrtho(-ro * x_aspect, ro * x_aspect, -ro * y_aspect, ro * y_aspect,
-              near, 10000000);
-    } else if (projection_type == VW_CENTRAL_PROJECTION) {
-      near = focal_ro;
-      far = ro < 0.0f ? -ro + model->dim : model->dim;
-      glFrustum(-focal_ro * x_aspect, focal_ro * x_aspect, -focal_ro * y_aspect,
-                focal_ro * y_aspect, near, far);
-    }
-  }
-}
-*/
 
 template <typename T, typename U>
 void RenderFFP<T, U>::DrawVertices(Scene<T, U> &scene) {
@@ -158,44 +109,14 @@ void RenderFFP<T, U>::DrawVertices(Scene<T, U> &scene) {
   if (!scene.IsEmpty()) {
     const Figure<T, U> &figure = scene.GetFigure();
     glColor3f(color[0], color[1], color[2]);
-    glPointSize(5.0);
-    std::cout << "vn = " << (GLsizei)figure.VerticesNumber() << '\n';
+    glPointSize(1.0);
     glDrawArrays(GL_POINTS, (GLint)0, (GLsizei)figure.VerticesNumber());
-    std::cout << "Error: " << glGetError() << '\n';
   }
 }
-
-/*
-static void draw_vertices(vw_model_t *model) {
-  float size, ro;
-  vw_point_t centre;
-  float color[4];
-  int vertex_type;
-
-  vertex_type = vw_scene_get_vertex_type();
-  if (vertex_type != VW_NONE_VERTEX) {
-    vw_scene_get_vertex_color(&color[0], &color[1], &color[2]);
-    glColor3f(color[0], color[1], color[2]);
-    vw_scene_get_camera(&centre[0], &centre[1], &centre[2]);
-    ro = sqrtf(powf(model->centre[0] - centre[0], 2.0) +
-               powf(model->centre[1] - centre[1], 2.0) +
-               powf(model->centre[2] - centre[2], 2.0));
-    ro = ro < 2.0f * model->dim ? 2.0f * model->dim : ro;
-    size = vw_scene_get_vertex_size();
-    glPointSize(1.0f + size * model->dim / ro);
-    if (vertex_type == VW_CIRCLE_VERTEX) {
-      glEnable(GL_POINT_SMOOTH);
-    } else {
-      glDisable(GL_POINT_SMOOTH);
-    }
-    glDrawArrays(GL_POINTS, (GLint)0, (GLsizei)model->number_of_vertices);
-  }
-}
-*/
 
 template <typename T, typename U>
 void RenderFFP<T, U>::DrawFaces(Scene<T, U> &scene) {
-  float color[4] = {0, 0, 0, 0};
+  float color[4] = {0, 0, 1, 0};
 
   if (!scene.IsEmpty()) {
     const Figure<T, U> &figure = scene.GetFigure();
@@ -205,41 +126,11 @@ void RenderFFP<T, U>::DrawFaces(Scene<T, U> &scene) {
     using fit = typename std::vector<Face<U>>::const_iterator;
 
     for (fit it = faces.begin(), last = faces.end(); it != last; ++it) {
-      glDrawElements(GL_LINE_LOOP, it->Size(), GL_UNSIGNED_INT, it->data());
+      glDrawElements(GL_LINE_LOOP, it->Size(), GL_UNSIGNED_INT, it->Data());
       // std::cout << "Error: " << glGetError() << '\n';
     }
   }
 }
-
-/*
-static void draw_faces(vw_model_t *model) {
-  float size, ro;
-  vw_point_t centre;
-  float color[4];
-
-  vw_scene_get_line_color(&color[0], &color[1], &color[2]);
-  glColor3f(color[0], color[1], color[2]);
-  size = vw_scene_get_line_size();
-  vw_scene_get_camera(&centre[0], &centre[1], &centre[2]);
-  ro = sqrtf(powf(model->centre[0] - centre[0], 2.0) +
-             powf(model->centre[1] - centre[1], 2.0) +
-             powf(model->centre[2] - centre[2], 2.0));
-  ro = ro < 2.0f * model->dim ? 2.0f * model->dim : ro;
-  glLineWidth(1.0f + size * 2.0f * model->dim / ro);
-  glEnable(GL_LINE_SMOOTH);
-  if (vw_scene_get_line_type() == VW_DASHED_LINE) {
-    glEnable(GL_LINE_STIPPLE);
-    glLineStipple(1, 0x00FF);
-  } else {
-    glDisable(GL_LINE_STIPPLE);
-  }
-  for (size_t i = 0; i < model->number_of_faces; ++i) {
-    glDrawElements(GL_LINE_LOOP, model->faces[i].number_of_idx, GL_UNSIGNED_INT,
-                   model->faces[i].idx);
-  }
-}
-
-*/
 
 template <typename T, typename U>
 void RenderFFP<T,U>::ResizeViewport(int width, int height) {
